90aedf4d1f13b793062b24c121fed4752abec593
Aaron Peron <aaron.peron@yahoo.com>
2014-11-13 13:54:04 -0800

    background

---
+++ public_html/js/resources.js
@@ -0,0 +1,29 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+           {name: "example", type:"image", src: "data/img/example.png"},
+           
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+           {name: "Aaronlevel01", type: "tmx", src: "data/map/Aaronlevel01.tmx"}
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+	 */
+];
