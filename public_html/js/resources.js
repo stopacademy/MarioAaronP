9f7a456348d8838d0d49a6c331e78a3c0a0daeee
Aaron Peron <aaron.peron@yahoo.com>
2014-11-17 14:19:17 -0800

    all most there

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -4,8 +4,7 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-           {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
-           
+
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -16,7 +15,7 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-           {name: "level01", type: "tmx", src: "data/map/level01.tmx"}
+
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -5,9 +5,7 @@ game.PlayScreen = me.ScreenObject.extend({
 	onResetEvent: function() {
 		// reset the score
 		game.data.score = 0;
-                
-                me.levelDirector.loadLevel("level01");
-                
+
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
