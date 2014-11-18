29f063ef0a27d6fbe19ea245b20c1b821dabc061
Aaron Peron <aaron.peron@yahoo.com>
2014-11-18 14:02:49 -0800

    settings

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -1,1 +1,14 @@
 // TODO
+game.PlayerEntity = me.Entity.extend({
+   init: function(x, y, setting){
+       settings.image = "mario";
+       settings.spritewidth = "128";
+       settings.spriteheigth = "128";
+       
+   },
+    
+    update:function(){
+        
+    }
+    
+});
--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -4,7 +4,9 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-
+           {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+           {name: "mario", type:"image", src: "data/img/player1.png"},
+           
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -15,7 +17,8 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-
+           {name: "Aaronlevel01", type: "tmx", src: "data/map/Aaronlevel01.tmx"}
+           
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
