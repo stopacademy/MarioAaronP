8a7ab9003359ee25624d49dfab384be6c8aec315
Aaron Peron <aaron.peron@yahoo.com>
2014-11-20 14:04:14 -0800

    control

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -1,27 +1,27 @@
 game.PlayScreen = me.ScreenObject.extend({
-	/**
-	 *  action to perform on state change
-	 */
-	onResetEvent: function() {
-		// reset the score
-		game.data.score = 0;
+    /**
+     *  action to perform on state change
+     */
+    onResetEvent: function() {
+        // reset the score
+        game.data.score = 0;
 
-            me.levelDirector.loadLevel("Aaronlevel01");
-            
-            var player = me.pool.pull("mario", 0, 420, {});
-            me.game.world.addChild(player, 1);
+        me.levelDirector.loadLevel("Aaronlevel01");
 
-		// add our HUD to the game world
-		this.HUD = new game.HUD.Container();
-		me.game.world.addChild(this.HUD);
-	},
+        var player = me.pool.pull("mario", 0, 420, {});
+        me.game.world.addChild(player, 3);
+        
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
 
-
-	/**
-	 *  action to perform when leaving this screen (state change)
-	 */
-	onDestroyEvent: function() {
-		// remove the HUD from the game world
-		me.game.world.removeChild(this.HUD);
-	}
+        // add our HUD to the game world
+        this.HUD = new game.HUD.Container();
+        me.game.world.addChild(this.HUD);
+    },
+    /**
+     *  action to perform when leaving this screen (state change)
+     */
+    onDestroyEvent: function() {
+        // remove the HUD from the game world
+        me.game.world.removeChild(this.HUD);
+    }
 });
