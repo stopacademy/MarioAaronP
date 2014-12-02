6278bdde6c5d2337e36c8bcfddb6712ed224df42
Aaron Peron <aaron.peron@yahoo.com>
2014-12-01 17:02:40 -0800

    animation

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -12,17 +12,32 @@ game.PlayerEntity = me.Entity.extend({
               }
        }]);
        
+       this.renderable.addAnimation("idle", [3]);
+       this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
+       
+       this.renderable.setCurrentAnimation("idle");
+       
        this.body.setVelocity(5, 20);
        
    },
     update: function(delta) {
         if(me.input.isKeyPressed("right")){
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            
         }else{
             this.body.vel.x = 0;
         }
         
+        if(this.body.vel.x !== 0){
+            if(!this.renderable.setCurrentAnimation("smallWalk")) {
+            this.renderable.setCurrentAnimation("smallWalk");
+        }
+        }else{
+            this.renderable.setCurrentAnimation("idle");
+        }
+        
         this.body.update(delta);
+        this._super(me.Entity, "update", [delta]);
         return true;
     }
   
